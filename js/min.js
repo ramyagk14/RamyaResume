40fdf6e9a4fb1e1d31467fde5491c7fc0ecc4820
ramyagk14 <ramyagk3@gmail.com>
2025-01-04 17:02:10 +0530

    [master] work experrience tab added

--- js/min.js
+++ js/min.js
@@ -57,4 +57,19 @@ jQuery(document).ready(function(){
 		},6000);
 	});
 
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
+
 });
